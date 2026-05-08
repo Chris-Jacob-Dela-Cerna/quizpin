d57bb9fd06e8d4cd1dc854ca9e3c850d706c3b63
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-08 10:45:08 +0800

    Feat: implement initial tests for checker

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -3,20 +3,37 @@ from checker import checker as ck
 
 
 def test_checker():
-    dic = {"a": "apple"}
-    assert ck.checker("a", dic) == "apple"
+    dct = {"a": "apple",}
+    assert ck.checker("a", dct) == "apple"
+    assert ck.checker("  a  ", dct) == "apple"
+    assert ck.checker("A", dct) == "apple"
+    assert ck.checker("b", dct) == None
+    assert ck.checker("", dct) == None
 
 
 def test_if_yes():
-    pass
+    assert ck.if_yes("y") == True
+    assert ck.if_yes("  y  ") == True
+    assert ck.if_yes("Y") == True
+    assert ck.if_yes("n") == False
+    assert ck.if_yes("") == False
 
 
 def test_check_quizzes():
-    pass
+    lst = ["fruits.csv"]
+    empty_lst = []
+    assert ck.check_quizzes(lst) == True
+    assert ck.check_quizzes(empty_lst) == False
+
 
 
 def test_check_score():
-    pass
+    total = 10
+    assert ck.check_score(10, total) == ("A perfect score, bravo!", "=========================")
+    assert ck.check_score(8, total) == ("What a great run!", "===================")
+    assert ck.check_score(5, total) == ("Well done! Solid work.", "========================")
+    assert ck.check_score(3, total) == ("You're getting there!", "=======================")
+    assert ck.check_score(0, total) == ("A great learning opportunity!", "===============================")
 
 
 def test_check_prefix():
