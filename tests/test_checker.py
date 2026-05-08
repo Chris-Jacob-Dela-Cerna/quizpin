c2c2848f558fb50b22c661ba52958afe3a45b791
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-05-08 10:15:50 +0800

    Feat: add initial structure for test_checker and add __init__.py to folders

---
+++ tests/test_checker.py
@@ -0,0 +1,27 @@
+
+from checker import checker as ck
+
+
+def test_checker():
+    dic = {"a": "apple"}
+    assert ck.checker("a", dic) == "apple"
+
+
+def test_if_yes():
+    pass
+
+
+def test_check_quizzes():
+    pass
+
+
+def test_check_score():
+    pass
+
+
+def test_check_prefix():
+    pass
+
+
+def test_check_name():
+    pass
